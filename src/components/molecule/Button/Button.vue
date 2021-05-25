b9119217a42dafeae52ced50d4c0d7a2fe4b71c7
Ming <ming.k@hotmail.com>
2021-05-26 07:30:28 +0800

    add component directories

---
+++ src/components/molecule/Button/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>THIS IS BUTTON</button>
+</template>
+
+<script>
+export default {
+  name: "Button"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/organism/Card/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from "../../molecule/Button/Button.vue"
+export default {
+  name: "Card",
+  components: {
+    Button,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
