113199b1ae8c3e5eaba4774cc1d6108cc8e64a17
CheeMinHao <cheemh3456@gmail.com>
2021-05-31 17:28:28 +0800

    feat(NavBar): added NavBar Design

--- src/components/organism/NavBar/NavBar.vue
+++ src/components/organism/NavBar/NavBar.vue
@@ -1,22 +1,12 @@
 <template>
-  <div class="bg-red-600">
-    <h1>
-      I AM NAVBAR
-    </h1>
-    -p------------------
-    <Button label="SUBMIT" />
-    <Button label="APPLY" :primary="true" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" :primary="true" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-    <Button label="SUBMIT" />
-  </div>
+  <nav className="bg-gray-300 p-5 flex flex-row justify-between">
+    <img src="https://i.imgur.com/hghfnW9.jpg" className="h-11" />
+    <div className="flex flex-row items-center gap-4">
+      <Button label="Cart" className="border-opacity-0 font-semibold" />
+      <Button label="Messages" className="border-opacity-0 font-semibold" />
+      <img src="https://i.imgur.com/hghfnW9.jpg" className="rounded-full h-11 w-11" />
+    </div>
+  </nav>
 
 </template>
 
