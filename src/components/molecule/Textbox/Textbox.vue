4488523440d2ad404e0291bcd69c804188be8898
EazyPau <poyizhi@gmail.com>
2021-06-02 08:07:52 +0800

    adding LoginPage and RegisterPage

--- src/components/molecule/Textbox/Textbox.vue
+++ src/components/molecule/Textbox/Textbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col items-center">
-    <label for="name" class="text-gray-600 pr-40 mt-1">{{ text }}</label>
+    <label for="name" class="text-gray-600 mt-3 h-auto w-52 text-left">{{ text }}</label>
     <input class="border-4 border-gray-400 rounded-lg p-1.5" type="text" placeholder="Name" id="name">
   </div>
 
