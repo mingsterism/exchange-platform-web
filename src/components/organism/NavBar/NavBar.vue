4f41d71251ccebc1323c0e831c4acaf95bff4b13
CheeMinHao <cheemh3456@gmail.com>
2021-06-10 14:57:46 +0800

    refactor(Textbox):modified Textbox Design and reactivity

--- src/components/organism/NavBar/NavBar.vue
+++ src/components/organism/NavBar/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav className="bg-gray-300 p-5 flex flex-row justify-between">
-    <img src="https://i.imgur.com/hghfnW9.jpg" className="h-11" />
+    <img src="https://i.imgur.com/hghfnW9.jpg" className="h-11" @click="onClick" />
     <div className="flex flex-row items-center gap-4">
       <Button label="Cart" className="border-opacity-0 font-semibold" />
       <Button label="Messages" className="border-opacity-0 font-semibold" />
@@ -16,6 +16,16 @@ export default {
   name: "NavBar",
   components: {
     Button
+  },
+
+  emits: ['click'],
+
+  setup({emit}) {
+    return {
+      onClick() {
+        emit(click);
+      }
+    }
   }
 }
 </script>
