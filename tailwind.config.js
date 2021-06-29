b7c33402f08b9be2a233a23b3b1e9d2c372a180a
EazyPau <poyizhi@gmail.com>
2021-06-29 23:57:09 +0800

    added displayUser and updateUser

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
 
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'clothes': "url('https://st3.depositphotos.com/17369252/i/600/depositphotos_372536484-stock-photo-fashion-clothes-rack-light-background.jpg')",
+       })
+    },
     maxWidth: {
       'sm': '18rem',
       'xs': '17rem',
