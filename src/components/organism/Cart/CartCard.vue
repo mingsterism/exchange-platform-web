d578e559af284c9bb03a726dcae0aefb72647527
EazyPau <poyizhi@gmail.com>
2021-10-04 11:43:30 +0800

    create add to cart function

---
+++ src/components/organism/Cart/CartCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- rectangular card -->
+    <div>
+      <Checkbox />
+    </div>
+  </div>
+</template>
+
+<script>
+import Checkbox from "../../molecule/Checkbox/Checkbox.vue";
+export default {
+  components: { Checkbox },
+};
+</script>
+
+<style></style>
--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,10 +1,22 @@
 <template>
-  
+  <div>
+      <div>
+          <h1>Shopping Cart</h1>
+      </div>
+      <div>
+          <!-- Cart cards -->
+          <CartCard />
+      </div>
+      <div>
+          <!-- checkout footer -->
+      </div>
+  </div>
 </template>
 
 <script>
+import CartCard from '../components/organism/Cart/CartCard.vue';
 export default {
-
+    components: {CartCard}
 }
 </script>
 
